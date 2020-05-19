2a7ea8c5c15027cbc40da159957cea4790db5f96
Glitch (theeverythingbot) <none>
2020-05-19 14:04:09 +0000

    First Commit

---
+++ commands/gifs/gif.js
@@ -0,0 +1,38 @@
+const fetch = require('node-fetch');
+const { tenorAPI } = require('../../config.json');
+const { Command } = require('discord.js-commando');
+
+module.exports = class GifCommand extends Command {
+  constructor(client) {
+    super(client, {
+      name: 'gif',
+      group: 'gifs',
+      aliases: ['search-gif', 'search-gifs'],
+      memberName: 'gif',
+      description: 'Provide a query and I will return a gif!',
+      throttling: {
+        usages: 1,
+        duration: 4
+      },
+      args: [
+        {
+          key: 'text',
+          prompt: 'What gif would you like to watch?',
+          type: 'string',
+          validate: text => text.length < 50
+        }
+      ]
+    });
+  }
+
+  run(message, { text }) {
+    fetch(`https://api.tenor.com/v1/random?key=${tenorAPI}&q=${text}&limit=1`)
+      .then(res => res.json())
+      .then(json => message.say(json.results[0].url))
+      .catch(e => {
+        message.say('Failed to find a gif that matched your query');
+        // console.error(e);
+        return;
+      });
+  }
+};
---
+++ index.js
@@ -0,0 +1,63 @@
+const { CommandoClient } = require('discord.js-commando');
+const { Structures } = require('discord.js');
+const path = require('path');
+const { prefix, token } = require('./config.json');
+
+Structures.extend('Guild', function(Guild) {
+  class MusicGuild extends Guild {
+    constructor(client, data) {
+      super(client, data);
+      this.musicData = {
+        queue: [],
+        isPlaying: false,
+        nowPlaying: null,
+        songDispatcher: null,
+        volume: 1
+      };
+      this.triviaData = {
+        isTriviaRunning: false,
+        wasTriviaEndCalled: false,
+        triviaQueue: [],
+        triviaScore: new Map()
+      };
+    }
+  }
+  return MusicGuild;
+});
+
+const client = new CommandoClient({
+  commandPrefix: prefix,
+  owner: 'your-discord-user-id' // change this to your Discord user ID
+});
+
+client.registry
+  .registerDefaultTypes()
+  .registerGroups([
+    ['music', 'Music Command Group'],
+    ['gifs', 'Gif Command Group'],
+    ['other', 'random types of commands group'],
+    ['guild', 'guild related commands']
+  ])
+  .registerDefaultGroups()
+  .registerDefaultCommands({
+    eval: false,
+    prefix: false,
+    commandState: false
+  })
+  .registerCommandsIn(path.join(__dirname, 'commands'));
+
+client.once('ready', () => {
+  console.log('Ready!');
+  client.user.setActivity(`${prefix}help`, {
+    type: 'WATCHING',
+    url: 'https://github.com/githubfan/TheEverythingBot'
+  });
+});
+
+client.on('guildMemberAdd', member => {
+  const channel = member.guild.channels.cache.find(ch => ch.name === 'general'); // change this to the channel name you want to send the greeting to
+  if (!channel) return;
+  channel.send(`Welcome ${member}!`);
+});
+
+client.login(token);
