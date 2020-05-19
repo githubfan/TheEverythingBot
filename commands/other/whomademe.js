4f53dd101f9f63435ad6a8f7eafc223d688a6370
Coding Master <codingmaster24@aol.com>
2020-05-19 15:20:50 +0100

    made some changes

--- commands/other/whomademe.js
+++ commands/other/whomademe.js
@@ -13,7 +13,7 @@ module.exports = class WhoMadeMeCommand extends Command {
 
   run(message) {
     message.say(
-      'Made by @hyperzone#1185 with :heart: full code is available on GitHub https://github.com/galnir/Master-Bot'
+      'Made by @githubfan#7548 with :heart: full code is available on GitHub https://github.com/githubfan/TheEverythingBot'
     );
   }
 };
